650281be98cc45e8913bf9f1ad030d707c3d0400
unknown <jjk3155@naver.com>
2024-07-01 20:35:25 +0900

    로또만들기 완

--- 자바스크립트/만들어보기/lotto.js
+++ 자바스크립트/만들어보기/lotto.js
@@ -1,29 +1,27 @@
-// const lotto = [];
-
-// for (let i = 0; i < 6; i++) {
-//     let num = parseInt(Math.random()*45+1)
-//     for (let j = 0; j < lotto.length; j++) {
-//         if (num === lotto[j]) {
-//             console.log(1);
-//             continue
-//         }
-//     }
-//     lotto.push(num)
-
-// }
-// console.log(lotto);
-
 const lotto = [];
+
+for (let i = 0; i < 6; i++) {
+    let num = parseInt(Math.random()*45+1)
+    for (let j = 0; j < lotto.length; j++) {
+        if (num === lotto[j]) {
+            continue
+        }
+    }
+    lotto.push(num)
+}
+console.log(lotto);
+
+const lotto2 = [];
 
 const result = [];
 
 for (let i = 1; i < 46; i++) {
-    lotto.push(i)
+    lotto2.push(i)
 }
 
 for (let i = 0; i < 6; i++) {
-    let num = parseInt(Math.random() * lotto.length-1);
-    const re = lotto[num];
+    let num = parseInt(Math.random() * lotto2.length-1);
+    const re = lotto2[num];
     result.push(re);
     lotto.splice(num, 1);
 }
